9aa2ba4e93047081a93fc3f5ae691de4cbe9c03a
Andrew Zimolzak (Dell Core i5) <andyzimolzak@gmail.com>
2017-11-08 11:58:34 -0500

    Reorganize and act like CSV.

--- get-xkcd.py
+++ get-xkcd.py
@@ -1,29 +1,24 @@
 import json
 import urllib.request
 import ssl
-from sys import argv
 
-comic_to_get = 615
-if len(argv) > 1:
-    comic_to_get = int(argv[1])
+unverified = ssl._create_unverified_context()
 
 def get_comic_by_num(n):
-    url = 'https://xkcd.com/' + str(comic_to_get) + '/info.0.json'
-    unverified = ssl._create_unverified_context()
+    url = 'https://xkcd.com/' + str(n) + '/info.0.json'
     response = urllib.request.urlopen(url, context = unverified)
     json_data = str(response.read(), encoding='utf-8')
     return json.loads(json_data)
 
+#### main ####
+
+print("comic_num,panels,words")
+
+comic_to_get = 615
 python_obj = get_comic_by_num(comic_to_get)
-
-#print(json.dumps(python_obj, sort_keys=True, indent=4))
-#print(python_obj.keys())
-# usu keys:
-# safe_title link alt news title num transcript month day img year
-
 n = python_obj['num']
 assert comic_to_get == n
-
 T = python_obj['transcript']
-L = T.split('\n\n')
-print("Comic " + str(n) + "\tEst. num. of panels:", len(L) - 1)
+panels = len(T.split('\n\n')) - 1
+words = len(T.split())
+print(str(n) + ',' + str(panels) + ',' + str(words))
